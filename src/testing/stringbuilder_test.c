e51bb70ef97aa3d4b19e4b5be60462cb61c35fa5
Andrew McCluskey <andrew@ajmccluskey.com>
2013-04-08 17:51:25 +1000

    Fix null termination in stringbuilder and refactor
    Fix an issue in stringbuilder whereby it was not null terminating
    strings correctly.  In applying this fix, a number of vaguely related
    changes are made.
    
    stringbuilder
     - Fill string buffer with null on creation and resize to ensure any
       characters not written to the string are null, and the buffer is
       therefore always null terminated.
     - Make sb_reset a function, not a macro
     - Add sb_resize function as reusuable and consistent means of resizing
       buffer
     - Add sb_double_size as a convenience function to double the buffer size
     - Make sb_append_ch a function, not a macro
     - Change resize strategy in sb_append_strn to keep doubling the new
       buffer size until the string to be appended will fit
    
    stringbuilder_test
     - Add check of cstring's length to _assert_sb_stats
     - Modify tests to match new code

--- src/testing/stringbuilder_test.c
+++ src/testing/stringbuilder_test.c
@@ -11,6 +11,10 @@ static void _assert_sb_stats(stringbuilder* sb, const char* str, int size, int r
         fprintf(stderr, "SB string (%s) does not match '%s'\n", sb_cstring(sb), str);
         exit(-1);
     }
+
+    if (strlen(str) != strlen(sb_cstring(sb))) {
+        fprintf(stderr, "CSTR size expected to be %d, but is %d\n", strlen(str), strlen(sb_cstring(sb)));
+    }
     
     if (size != sb->size)   {
         fprintf(stderr, "SB Size (%d) does not match required size (%d)\n", sb->size, size);
@@ -26,6 +30,10 @@ static void _assert_sb_stats(stringbuilder* sb, const char* str, int size, int r
 DEFINE_TEST_FUNCTION {  
     char *cstr;
     stringbuilder* sb = sb_new_with_size(1);
+    if (0 != strlen(sb->cstr)) {
+        fprintf(stderr, "CSTR expected to have length 0, has length %d\n", strlen(sb->cstr));
+        exit(-1);
+    }
     
     sb_append_ch(sb, 'H');
     sb_append_ch(sb, 'e');
@@ -41,7 +49,7 @@ DEFINE_TEST_FUNCTION {
     sb_append_ch(sb, 'd');
     sb_append_ch(sb, '!');
     
-    _assert_sb_stats(sb, "Hello, World!", strlen("Hello, World!")+1, 7);
+    _assert_sb_stats(sb, "Hello, World!", 16, 4);
     
     cstr = sb_make_cstring(sb);
     if (strcmp(cstr, "Hello, World!"))  {
@@ -51,20 +59,21 @@ DEFINE_TEST_FUNCTION {
     free(cstr);
         
     sb_reset(sb);
+    _assert_sb_stats(sb, "", 16, 4);
     
     sb_append_ch(sb, 'H');
     sb_append_ch(sb, 'i');
     sb_append_ch(sb, '!');
-    _assert_sb_stats(sb, "Hi!", strlen("Hello, World!")+1, 7);
+    _assert_sb_stats(sb, "Hi!", 16, 4);
     
     sb_append_str(sb, "This is a longer string that I am appending, doncha know");
-    _assert_sb_stats(sb, "Hi!This is a longer string that I am appending, doncha know", 71, 8);
+    _assert_sb_stats(sb, "Hi!This is a longer string that I am appending, doncha know", 64, 5);
     
     sb_append_ch(sb, '?');
-    _assert_sb_stats(sb, "Hi!This is a longer string that I am appending, doncha know?", 71, 8);
+    _assert_sb_stats(sb, "Hi!This is a longer string that I am appending, doncha know?", 64, 5);
     
     sb_append_strf(sb, " And %s %s!", "even", "longer");
-    _assert_sb_stats(sb, "Hi!This is a longer string that I am appending, doncha know? And even longer!", 79, 9);
+    _assert_sb_stats(sb, "Hi!This is a longer string that I am appending, doncha know? And even longer!", 128, 6);
     
     sb_destroy(sb, 1);
     return 0;
